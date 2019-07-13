20f71f1b26a39afbf605802c5b93b15fad0be2c0
zshroot <52833706+zshroot@users.noreply.github.com>
2019-07-13 12:46:48 -0300

    Add files via upload

---
+++ miner.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt install git -y
+sudo apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev make g++ -y
+git clone https://github.com/tpruvot/cpuminer-multi.git
+cd cpuminer-multi/
+./build.sh
+cd cpuminer-multi/
+./cpuminer -a xevan -o stratum+tcp://bsd.suprnova.cc:8686 -u lucas0902.bsd -p x 
